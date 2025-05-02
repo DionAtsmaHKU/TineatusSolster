d48df5ee164d758fb66fb60dbe387c3d891b679c
Dion Atsma <dion.atsma@student.hku.nl>
2025-05-02 15:24:48 +0200

    Pick your major, player stands still

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,21 @@
 using System;
 using UnityEngine;
+using Yarn.Unity;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] DialogueRunner runner;
     public float moveSpeed = 5f;
     private CharacterController controller;
-
+    private bool inDialogue = false;
+    
     public static event Action OnInteract;
+
+    private void Awake()
+    {
+        runner.onDialogueComplete.AddListener(EnterExitDialogue);
+        runner.onDialogueStart.AddListener(EnterExitDialogue);
+    }
 
     void Start()
     {
@@ -15,6 +24,9 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (inDialogue)
+            return;
+
         Move();
 
         if (Input.GetKeyDown(KeyCode.E))
@@ -31,5 +43,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = new Vector3(moveX, 0, moveZ).normalized * moveSpeed;
 
         controller.Move(move * Time.deltaTime); // Move while respecting collisions
+    }
+
+    public void EnterExitDialogue()
+    {
+        inDialogue = !inDialogue;
     }
 }
