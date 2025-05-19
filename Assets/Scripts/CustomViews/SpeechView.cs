90290e6c591ee1bebbe50ce5c6cf1c07653eb98d
Dion Atsma <dion.atsma@student.hku.nl>
2025-05-19 16:13:24 +0200

    speech view update

--- Assets/Scripts/CustomViews/SpeechView.cs
+++ Assets/Scripts/CustomViews/SpeechView.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 using System;
+using UnityEngine.UI;
 
 public class SpeechView : LineView
 {
+    public HorizontalLayoutGroup layoutGroup;
+
     public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
     {
-        if (dialogueLine.CharacterName != null)
-            base.RunLine(dialogueLine, onDialogueLineFinished);
+        if (dialogueLine.CharacterName == null || dialogueLine.CharacterName == "Star")
+            return;
+
+        if (dialogueLine.CharacterName == "Sasha")
+        {
+            layoutGroup.padding.right = 80;
+            layoutGroup.padding.left = 0;
+        }
+        else 
+        {
+            layoutGroup.reverseArrangement = false;
+            layoutGroup.padding.right = 0;
+            layoutGroup.padding.left = 80;
+        }
+
+        base.RunLine(dialogueLine, onDialogueLineFinished);
     }
 }
