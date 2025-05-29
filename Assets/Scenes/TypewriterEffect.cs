65b5b06a686838f57dffb563a437a8e6b6b4aaa4
benjesta <anna.m.majoor@gmail.com>
2025-05-29 13:14:10 +0200

    edited StarScene

---
+++ Assets/Scenes/TypewriterEffect.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+//GPTCODE
+
+public class TypewriterEffect : MonoBehaviour
+{
+    [System.Serializable]
+    public class TextboxData
+    {
+        public TextMeshProUGUI textBox;
+        [TextArea] public string fullText;
+        public Transform triggerPoint;  // position near the textbox
+        public float triggerDistance = 5f;
+        [HideInInspector] public bool hasStarted = false;
+    }
+
+    public List<TextboxData> textboxes = new List<TextboxData>();
+    public Transform player;
+    public float letterDelay = 0.05f;
+
+    void Update()
+    {
+        foreach (TextboxData data in textboxes)
+        {
+            if (!data.hasStarted && Vector3.Distance(player.position, data.triggerPoint.position) <= data.triggerDistance)
+            {
+                data.hasStarted = true;
+                StartCoroutine(TypeText(data));
+            }
+        }
+    }
+
+    IEnumerator TypeText(TextboxData data)
+    {
+        data.textBox.text = "";
+        foreach (char letter in data.fullText)
+        {
+            data.textBox.text += letter;
+            yield return new WaitForSeconds(letterDelay);
+        }
+    }
+}
