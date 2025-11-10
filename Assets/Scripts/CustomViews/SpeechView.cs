7119c5734b50ef1613b77714d10f47e2a6745416
SCOTTIE. <76739442+benjesta@users.noreply.github.com>
2025-11-10 18:07:42 +0100

    just fuckin stage evryhting

--- Assets/Scripts/CustomViews/SpeechView.cs
+++ Assets/Scripts/CustomViews/SpeechView.cs
@@ -4,34 +4,100 @@ using UnityEngine;
 using Yarn.Unity;
 using System;
 using UnityEngine.UI;
+using TMPro;
 
 public class SpeechView : LineView
 {
     public DialogueAudio dialogueAudio;
     public HorizontalLayoutGroup layoutGroup;
+	
+	public GameObject DialogueBubble;        
+    public GameObject SashaDialogueBubble;
+	
+	//Bubbles of the Majors
+	public GameObject PsyDialogueBubble;
+	public GameObject TechDialogueBubble;
+	public GameObject ArtDialogueBubble;
+	
+    public TextMeshProUGUI DefaultText;
+    public TextMeshProUGUI SashaText;
+	
+    public TextMeshProUGUI PsyText;
+    public TextMeshProUGUI TechText;
+    public TextMeshProUGUI ArtText;
 
     public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
     {
-        if (dialogueLine.CharacterName == null || dialogueLine.CharacterName == "Star" || dialogueLine.CharacterName == "Narrator")
-            return;
+        if (dialogueLine.CharacterName == null || dialogueLine.CharacterName == "Star")
+            return;	        
 
-        if (dialogueLine.CharacterName == "Sasha")
+		
+        if (dialogueLine.CharacterName == "SASHA")
         {
+            DialogueBubble.SetActive(false);
+            SashaDialogueBubble.SetActive(true);
+			PsyDialogueBubble.SetActive(false);
+			TechDialogueBubble.SetActive(false);
+			ArtDialogueBubble.SetActive(false);
+			lineText = SashaText;
+            layoutGroup = SashaDialogueBubble.GetComponent<HorizontalLayoutGroup>();
+            layoutGroup.padding.left = 0;
             layoutGroup.padding.right = 80;
+        }
+        else if (dialogueLine.CharacterName == "PSY")
+        {
+            DialogueBubble.SetActive(false);
+			SashaDialogueBubble.SetActive(false);
+            PsyDialogueBubble.SetActive(true);
+			TechDialogueBubble.SetActive(false);
+			ArtDialogueBubble.SetActive(false);
+			
+			lineText = PsyText;
+            layoutGroup = PsyDialogueBubble.GetComponent<HorizontalLayoutGroup>();
+			
+            layoutGroup.padding.right = 0;
+            layoutGroup.padding.left = 0;
+        }	
+        else if (dialogueLine.CharacterName == "ART" )
+        {
+			DialogueBubble.SetActive(false);
+			SashaDialogueBubble.SetActive(false);
+			PsyDialogueBubble.SetActive(false);
+			TechDialogueBubble.SetActive(false);
+            ArtDialogueBubble.SetActive(true);
+			
+			lineText = ArtText;
+            layoutGroup = ArtDialogueBubble.GetComponent<HorizontalLayoutGroup>();
+			
+            layoutGroup.padding.right = 0;
             layoutGroup.padding.left = 0;
         }
-		
-        if (dialogueLine.CharacterName == "Psy" || dialogueLine.CharacterName == "Art" || dialogueLine.CharacterName == "Tech")
+		else if (dialogueLine.CharacterName == "TECH")
         {
+            DialogueBubble.SetActive(false);
+			SashaDialogueBubble.SetActive(false);
+			PsyDialogueBubble.SetActive(false);
+            TechDialogueBubble.SetActive(true);
+			ArtDialogueBubble.SetActive(false);
+			
+			lineText = TechText;
+            layoutGroup = TechDialogueBubble.GetComponent<HorizontalLayoutGroup>();
+			
             layoutGroup.padding.right = 0;
             layoutGroup.padding.left = 0;
         }		
-			
-        else 
+        else
         {
-            layoutGroup.reverseArrangement = false;
-            layoutGroup.padding.right = 0;
+            DialogueBubble.SetActive(true);
+            SashaDialogueBubble.SetActive(false);
+			PsyDialogueBubble.SetActive(false);
+			TechDialogueBubble.SetActive(false);
+			ArtDialogueBubble.SetActive(false);
+			lineText = DefaultText;
+
+            layoutGroup = DialogueBubble.GetComponent<HorizontalLayoutGroup>();
             layoutGroup.padding.left = 80;
+            layoutGroup.padding.right = 0;
         }
         base.RunLine(dialogueLine, onDialogueLineFinished);
     }
